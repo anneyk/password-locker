ae0faf59aa7b2e454cd5ebe5dbc5bf8e38582a3d
Annetkhavere <anneywan5@gmail.com>
2022-04-24 16:49:01 +0300

    create a delete_credentials function

--- run.py
+++ run.py
@@ -7,3 +7,15 @@ def create_user(app_name, user_name, password):
   new_user = Credentials(app_name, user_name, password)
   return new_user
 
+def store_credentials(credential):
+  '''
+  function to store credentials
+  '''
+  credential.store_credentials()
+
+def delete_credentials(credential):
+  '''
+  function to delete credentials
+  '''
+  credential.delete_credentials()
+  
