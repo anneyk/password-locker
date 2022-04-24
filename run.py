59e5b1bf7c95ca26a0c21515e09468e13f59e25a
Annetkhavere <anneywan5@gmail.com>
2022-04-24 22:27:07 +0300

    create a  variable known as short_code

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from tkinter import Y
 from password import Credentials
 import random
 
@@ -50,19 +51,24 @@ def main():
     print("\n")
 
   print(f"Hello {user_name}, What would you like to do ")
+  print("\n")
+ 
+  while True:
+    print("Use these short codes : cc - create new credential, dc - display all credentials, ex - exit credential list ")
+    
+    short_code = input().lower()
+   
+  # Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*&^()"
 
-
-  Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*&^()"
-
-  while 1:
-    password_len = int(input("What length would you like your password to be: "))
-    password_count = int(input("How many passwords would you like to generate: "))
-    for x in range(0, password_count):
-      password = ""
-      for x in range(0, password_len):
-        password_char = random.choice(Chars)
-        password = password + password_char
-      print("Here is your random password: ", password)
+  # while 1:
+  #   password_len = int(input("What length would you like your password to be: "))
+  #   password_count = int(input("How many passwords would you like to generate: "))
+  #   for x in range(0, password_count):
+  #     password = ""
+  #     for x in range(0, password_len):
+  #       password_char = random.choice(Chars)
+  #       password = password + password_char
+  #     print("Here is your random password: ", password)
 
 
 if __name__ == '__main__':
