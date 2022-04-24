7fc4b2630d1f8066d166b199708ba4abbaf7ea00
Annetkhavere <anneywan5@gmail.com>
2022-04-24 13:17:54 +0300

    create an empty credentials_list

--- password.py
+++ password.py
@@ -2,7 +2,10 @@ class Credentials:
   '''
   Class creates new instance of user password locker account class
   '''
+  credentials_list = [] #store our credentials here by appendng after executing store_credentials()
+  
   def __init__(self, app_name, user_name, password):
     self.app_name = app_name
     self.user_name = user_name
     self.password = password
+
