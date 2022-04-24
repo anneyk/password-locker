f91c83bfae78635eda759461347f4ce72e94b8d9
Annetkhavere <anneywan5@gmail.com>
2022-04-24 13:41:10 +0300

    create test case test_store_multiple_credentials to test if we can store mulple account credentials

--- password_test.py
+++ password_test.py
@@ -16,12 +16,22 @@ class TestCredentials (unittest.TestCase):
     self.assertEqual(self.new_credentials.user_name,"Hellen")
     self.assertEqual(self.new_credentials.password, "myPassword")
 
-  def test_store_account_credentials(self):
+  def test_store_credentials(self):
     '''
-    test_store_account_credentials test case to test if the credentials object is stored into the credentials_list
+    test_store_credentials test case to test if the credentials object is stored into the credentials_list
     '''
     self.new_credentials.store_credentials() #store the new credentials
     self.assertEqual(len(Credentials.credentials_list),1)
+
+  def test_store_multiple_credentials(self):
+    '''
+    test_store_mutiple_credentials to check if we can store multiple credentials objects to our credentials_list
+    '''
+    self.new_credentials.store_credentials()
+    test_credentials = Credentials("Twitter", "Hellie_","myTwitterPassword") #new account credentials
+    test_credentials.store_credentials()
+    self.assertEqual(len(Credentials.credentials_list),2)
+
 
 if __name__ == '__main__':
   unittest.main()
