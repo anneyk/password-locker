2d6405ec27d0eead0b8b491987cc20eaa2eee72a
Annetkhavere <anneywan5@gmail.com>
2022-04-24 12:06:56 +0300

    test if our objects are being instantiated correctly

---
+++ password_test.py
@@ -0,0 +1,19 @@
+import unittest #import the unittest module
+from password import UserPasswordLockerAccount #import the userpassword locker accouunt class
+
+class TestUserPasswordLockerAccount (unittest.TestCase):
+  def setUp(self):
+    '''
+    Set up method to run before each test cases
+    '''
+    self.new_password_locker_account = UserPasswordLockerAccount("Hellen","myPassword") #create a password locker account object
+
+  def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+    self.assertEqual(self.new_password_locker_account.user_name,"Hellen")
+    self.assertEqual(self.new_password_locker_account.password, "myPassword")
+
+if __name__ == '__main__':
+  unittest.main()
