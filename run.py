6b77b77fb507bd100eb86670d281a295569933e1
Annetkhavere <anneywan5@gmail.com>
2022-04-24 23:19:20 +0300

    create new credentials and save them

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
-from tkinter import Y
 from password import Credentials
 import random
 
-def create_user(app_name, user_name, password):
+def create_new_credentials(app_name, user_name, password):
   '''
-  function to create new password locker user
+  function to create new user credentials
   '''
-  new_user = Credentials(app_name, user_name, password)
-  return new_user
+  new_credentials = Credentials(app_name, user_name, password)
+  return new_credentials
 
 def store_credentials(credential):
   '''
@@ -34,7 +33,7 @@ def main():
   if first_message == "Y":
     print("Great!")
     print('\n')
-    user_name = input("Please enter your username: ")
+    user_name = input("Please enter your username to login: ")
     password = input("Please enter your password: ")
     print("\n")
     print("Login successful!")
@@ -50,25 +49,47 @@ def main():
     print("account successfully created!")
     print("\n")
 
-  print(f"Hello {user_name}, What would you like to do ")
+  print(f"Hello {user_name}, What would you like to do? ")
   print("\n")
  
   while True:
     print("Use these short codes : cc - create new credential, dc - display all credentials, ex - exit credential list ")
     
     short_code = input().lower()
-   
-  # Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*&^()"
 
-  # while 1:
-  #   password_len = int(input("What length would you like your password to be: "))
-  #   password_count = int(input("How many passwords would you like to generate: "))
-  #   for x in range(0, password_count):
-  #     password = ""
-  #     for x in range(0, password_len):
-  #       password_char = random.choice(Chars)
-  #       password = password + password_char
-  #     print("Here is your random password: ", password)
+    if short_code == "cc":
+      print("New Credentials")
+      print("*"*20)
+
+      print("App name e.g Twitter, Instagram ....")
+      app_name = input()
+
+      print("Username ....")
+      user_name = input()
+
+      print("Enter password manually(m) or autogenerate(a)")
+      password = input("pick (m/a): ")
+
+      if password == "m":
+        print("Enter Password Manually")
+        password = input()
+
+      else:
+        Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*&^()"
+        password_len = int(input("What length would you like your password to be: "))
+        password_count = int(input("How many passwords would you like to generate: "))
+        for x in range(0, password_count):
+            password = ""
+            for x in range(0, password_len):
+              password_char = random.choice(Chars)
+              password = password + password_char
+              print("\n")
+            print("Here is your random password: ", password)
+       
+        store_credentials(create_new_credentials(app_name,user_name,password)) #create and save new credentials
+        print("\n")
+        print(f"New Credentials {app_name} {user_name} {password} created")
+        print("\n")
 
 
 if __name__ == '__main__':
