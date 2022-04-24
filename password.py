ba55ce896b4b1f51c1b96fc480fe0684eec61576
Annetkhavere <anneywan5@gmail.com>
2022-04-24 11:44:56 +0300

    add the def __init__ method with variables app_name user_name and password

--- password.py
+++ password.py
@@ -2,4 +2,7 @@ class UserPasswordLockerAccount:
   '''
   Class creates new instance of user password locker account class
   '''
-  
+  def __init__(self, app_name, user_name, password):
+    self.app_name = app_name
+    self.user_name = user_name
+    self.password = password
