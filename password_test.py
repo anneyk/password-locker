28a34522d0b53c9fe517839eb4f17b988dfdf0d3
Annetkhavere <anneywan5@gmail.com>
2022-04-24 16:26:35 +0300

    create a test case to check if we receive the list of th stored credentials

--- password_test.py
+++ password_test.py
@@ -48,6 +48,11 @@ class TestCredentials (unittest.TestCase):
     self.new_credentials.delete_credentials()#Delete credentials object
     self.assertEqual(len(Credentials.credentials_list),1)
 
+  def test_display_all_credentials(self):
+    '''
+    method that returns a list of all credentials saved
+    '''
+    self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
 
 
 if __name__ == '__main__':
